a5dccb6994d6e25a1f789af71b40df1a17c6878f
Jiri Bruchanov <jbruchanov@gmail.com>
2025-09-24 09:31:04 +0100

    KS-19 Update android, compose/target 36 related issues

--- sample/shared/src/commonMain/kotlin/com/test/kostra/appsample/SampleScreen.kt
+++ sample/shared/src/commonMain/kotlin/com/test/kostra/appsample/SampleScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -70,6 +71,7 @@ fun SampleScreen(extraContent: @Composable ColumnScope.() -> Unit = {}) = with(S
     Box(
         modifier = Modifier
             .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
             .padding(spacing),
     ) {
         Column(
