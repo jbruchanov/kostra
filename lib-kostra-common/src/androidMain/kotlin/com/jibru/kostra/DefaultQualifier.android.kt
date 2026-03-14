641e37abbb873bc103edb163e5c7fe0a0878e0a7
Jiri Bruchanov <jbruchanov@gmail.com>
2026-03-14 15:15:22 +0000

    KS-27 Better qualifier provider

--- lib-kostra-common/src/androidMain/kotlin/com/jibru/kostra/DefaultQualifier.android.kt
+++ lib-kostra-common/src/androidMain/kotlin/com/jibru/kostra/DefaultQualifier.android.kt
@@ -3,8 +3,19 @@ package com.jibru.kostra
 import android.content.res.Resources
 import java.util.Locale as JvmLocale
 
+private fun defaultDpi(resources: Resources = Resources.getSystem()): KDpi = KDpi.getClosest(resources.displayMetrics.density)
+private fun jvmDefaultLocale() = JvmLocale.getDefault().let { KLocale(it.language, it.country.takeIf { code -> code.isEmpty() || code.length == 2 }) }
+private fun androidDefaultLocale(resources: Resources = Resources.getSystem()) =
+    resources.configuration.locales.let { if (it.size() > 0) KLocale(it[0].language, it[0].country) else null }
+
 actual fun defaultQualifiers(): KQualifiers = KQualifiers(
-    locale = JvmLocale.getDefault().let { KLocale(it.language, it.country.takeIf { code -> code.isEmpty() || code.length == 2 }) },
-    //taken from JVM LocalDensity
-    dpi = KDpi.getClosest(Resources.getSystem().displayMetrics.density),
+    locale = androidDefaultLocale() ?: jvmDefaultLocale(),
+    dpi = defaultDpi(),
 )
+
+fun defaultQualifiers(resources: Resources): KQualifiers {
+    return KQualifiers(
+        locale = androidDefaultLocale(resources) ?: jvmDefaultLocale(),
+        dpi = defaultDpi(resources),
+    )
+}
--- lib-kostra-compose/src/commonMain/kotlin/com/jibru/kostra/compose/Qualifiers.kt
+++ lib-kostra-compose/src/commonMain/kotlin/com/jibru/kostra/compose/Qualifiers.kt
@@ -1,6 +1,16 @@
 package com.jibru.kostra.compose
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import com.jibru.kostra.DefaultQualifiersProvider
+import com.jibru.kostra.KQualifiers
 
 val LocalQualifiers = compositionLocalOf { DefaultQualifiersProvider.current }
+
+@Composable
+fun ProvideLocalQualifiers(qualifiers: KQualifiers = DefaultQualifiersProvider.current, content: @Composable () -> Unit) {
+    CompositionLocalProvider(LocalQualifiers provides qualifiers) {
+        content()
+    }
+}
